cd22346d1e5f9d80978caff496013e951ee12404
mrkidney <azweig@stanford.edu>
2016-07-31 15:42:14 -0700

    the python

--- language_evolution.py
+++ language_evolution.py
@@ -2,6 +2,18 @@ import numpy
 from random import random
 import collections
 
+def randomChoice(counter){
+	words = []
+	counts = []
+	for word in counter:
+		words.append(word)
+		counts.append(counter[word])
+	total = float(sum(counts))
+	normal_counts = [i/total for i in counts]
+
+	return(numpy.random.choice(words, 5, replace = True, p = normal_counts))
+}
+
 class Agent(object):
 
 	def __init__(self, caste, lexicon):
@@ -14,24 +26,22 @@ class Agent(object):
 		LOSE = (2 if self.caste == 0 else 1)
 		BASE_COPY = .2
 
-		words = []
-		counts = []
-		for word, count in agent.lexicon:
-			words.append(word)
-			counts.append(count)
-		total = float(sum(counts))
-		normal_counts = [i/total for i in counts]
+		chosen = randomChoice(agent.lexicon)
 
-		chosen = numpy.random.choice(words, 5, replace = False, p = normal_counts)
-		for word, count in common:
+		for word in chosen:
 			if word not in self.lexicon.keys():
-				return 0
+				if BASE_COPY * MOD > random():
+					print "WOOSH"
 
 castes = [0,1,2]
 lowerLexicon = collections.Counter()
 lowerLexicon["hello"] = 3
 lowerLexicon["goodbye"] = 2
 
+alice = Agent(0, lowerLexicon)
+bob = Agent(0, lowerLexicon)
+
+print(bob.talk(alice))
+
 print(lowerLexicon.most_common(2))
 print(lowerLexicon.keys())
-print(numpy.random.choice(4, 3, p = [.1,.8,.1,0]))
